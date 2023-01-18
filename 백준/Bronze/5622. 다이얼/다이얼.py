baf545d7a405e15633779bdc019bdcb3c0720da1
LEE Minwook <djqmflsk@naver.com>
2023-01-18 16:54:26 +0900

    [Bronze II] Title: 다이얼, Time: 112 ms, Memory: 34804 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,25 +1,10 @@
 import string
-
-s = list(input())
-t = 0
-abc = string.ascii_uppercase
-
+al = string.ascii_uppercase
+nums = '22233344455566677778889999'
+tr = str.maketrans(al,nums)
+s = input()
+t = len(s)
+s = s.translate(tr)
 for i in s:
-    if i in abc[0:3]: # 2
-        t += 3
-    elif i in abc[3:6]: # 3
-        t += 4
-    elif i in abc[6:9]: # 4
-        t += 5
-    elif i in abc[9:12]: # 5
-        t += 6
-    elif i in abc[12:15]: # 6
-        t += 7    
-    elif i in abc[15:19]: # 7
-        t += 8
-    elif i in abc[19:22]: # 8
-        t += 9
-    elif i in abc[22:26]: # 9
-        t += 10
-
+    t += int(i)
 print(t)
