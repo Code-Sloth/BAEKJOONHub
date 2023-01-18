f9a5d7a9c62f9e8037ca73d82cb6c371b2a788af
LEE Minwook <djqmflsk@naver.com>
2023-01-18 16:28:29 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -3,10 +3,9 @@ for _ in range(int(input())):
     n = int(input())
     li = list(range(1,n+1))
     li2 = [0] * n
-    li2[0] += 1
     
     for j in range(floor):
-        for i in range(1,n):
+        for i in range(n):
             li2[i] = sum(li[:i+1])
         li = li2.copy()
-    print(li[n-1])
+    print(li[-1])
