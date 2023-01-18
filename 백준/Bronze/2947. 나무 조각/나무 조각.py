2a27be8126be799c9637b31b17d32d6c8f600511
LEE Minwook <djqmflsk@naver.com>
2023-01-18 14:15:32 +0900

    [Bronze I] Title: 나무 조각, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2947. 나무 조각/나무 조각.py
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -1,10 +1,8 @@
 nums = list(map(int,input().split()))
 
-while 1:
+while nums != sorted(nums):
     for i in range(4):
         if nums[i] > nums[i+1]:
             nums.insert(i,nums[i+1])
             del nums[i+2]
             print(*nums)
-    if nums == sorted(nums):
-        break
