cfe95a255c662e7c14d9b109ab99865fdcb31283
LEE Minwook <djqmflsk@naver.com>
2023-01-22 10:28:16 +0900

    [Silver V] Title: 단어 정렬, Time: 932 ms, Memory: 33708 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,10 @@
+li = []
+di = {}
+
+for _ in range(int(input())):
+    li.append(input().strip())
+li = sorted(li)
+
+for i in li:
+    di[i] = len(i)
+print(*sorted(di.keys(),key = lambda x:di[x]),sep='\n')
