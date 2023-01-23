5c9ac360b5e2504ad855f6bde85cc9295db0553f
LEE Minwook <djqmflsk@naver.com>
2023-01-23 14:30:16 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8724 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,13 +1,11 @@
 import sys
-
 input = sys.stdin.readline
 
-n = int(input())
+
 li = [0] * 10001
 
-for i in range(n):
-    num = int(input())
-    li[num] += 1
+for i in range(int(input())):
+    li[int(input())] += 1
 
 for j in range(10001):
     if li[j] != 0:
