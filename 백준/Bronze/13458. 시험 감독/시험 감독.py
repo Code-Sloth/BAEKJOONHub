371395658f3a397b67dbc1673d0e499da90eae3e
LEE Minwook <djqmflsk@naver.com>
2023-01-24 19:54:22 +0900

    [Bronze II] Title: 시험 감독, Time: 704 ms, Memory: 152040 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -5,15 +5,11 @@ import math
 n = int(input())
 nums = list(map(int,input().split()))
 b, c = map(int,input().split())
-li = [0] * n
 total = 0
 
 for i in range(n):
-    li[i] = nums[i] - b
     total += 1
+    if nums[i] - b > 0:
+        total += math.ceil((nums[i]-b)/c)
 
-for j in li:
-    if j > 0:
-        total += math.ceil(j/c)
-        
 print(total)
