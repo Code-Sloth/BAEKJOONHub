a99648d62c625ed5c75994232e352d0380aeccf7
LEE Minwook <djqmflsk@naver.com>
2023-01-30 14:57:01 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.py
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.py
@@ -1,15 +1,5 @@
-
 import sys
 input = sys.stdin.readline
 
 li = input().split()
-a, b = list(map(int,list(li[0]))), list(map(int,list(li[1])))
-
-t = 0
-if len(li[0]) > len(li[1]):
-    for i in range(len(li[1])):
-        t += b[i] * sum(a)
-else:
-    for i in range(len(li[0])):
-        t += a[i] * sum(b)
-print(t)
+print(sum(map(int,li[0]))*sum(map(int,li[1])))
