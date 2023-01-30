f6ca15f1ef08a2ec08620ad9b68a18c0f6e1105d
LEE Minwook <djqmflsk@naver.com>
2023-01-30 16:37:11 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,14 +3,14 @@ input = sys.stdin.readline
 
 n, k = map(int,input().split())
 li = [list(map(int,input().split())) for i in range(n)]
-
 li2 = sorted(li,key = lambda x : (x[1],x[2],x[3]),reverse = True)
+t = 1
 
-if li[0][0] == k:print(1)
-else:
-    t = 1
-    for i in range(1,len(li2)+1):
+for i in range(1,len(li2)+1):
+    try:
         if li2[i][1:] != li2[i-1][1:]:
             t += 1
         if li2[i][0] == k:break
+    except:
+        t = 1
 print(t)
