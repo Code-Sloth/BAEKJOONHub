ce9f40150fdfcd63dc7b818f9866a23a89d8d9c9
LEE Minwook <djqmflsk@naver.com>
2023-01-30 21:15:41 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 34092 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+for _ in range(int(input())):
+    n, m = map(int,input().split())
+    li = deque(list(map(int,input().split())))
+    t = 0
+
+    while li:
+        mx = max(li)
+        st = li.popleft()
+        m -= 1
+
+        if st == mx:
+            t += 1
+            if m < 0:
+                print(t)
+                break
+        else:
+            li.append(st)
+            if m < 0:
+                m = len(li)-1
