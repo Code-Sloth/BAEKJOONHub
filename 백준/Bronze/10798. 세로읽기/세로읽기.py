07470af490f0eab18637789075c25f3ccc71929a
LEE Minwook <djqmflsk@naver.com>
2023-01-30 20:48:46 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -2,9 +2,8 @@ import sys
 input = sys.stdin.readline
 
 li = [list(input().strip()) for i in range(5)]
-max_li = max(map(len,li))
 
-for i in range(max_li):
+for i in range(15):
     for j in range(5):
         try:
             print(li[j][i],end='')
