34143edf4869cab76dc42857aa3ab99472a6e429
LEE Minwook <djqmflsk@naver.com>
2023-01-31 20:56:35 +0900

    [Silver V] Title: 국회의원 선거, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -2,15 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-li = [int(input()) for _ in range(n)]
-if n == 1:print(0)
+num = int(input())
+li = [int(input()) for _ in range(n-1)]
+if not li:print(0)
 else:
     t = 0
-    while 1:
-        li = [li[0]] + sorted(li[1:],reverse=True)
-        if li[0] <= li[1]:
-            li[1] -= 1
-            li[0] += 1
-            t += 1
-        if li[0] == max(li) and li[0] not in li[1:]:break
+    li.sort()
+    while num <= li[-1]:
+        li[-1] -= 1
+        num += 1
+        t += 1
+        li.sort()
     print(t)
