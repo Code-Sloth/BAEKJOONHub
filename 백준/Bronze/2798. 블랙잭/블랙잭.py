5037c1a6fb47adada2dd03cf03b6f0a783bb6e1e
LEE Minwook <djqmflsk@naver.com>
2023-02-01 11:01:53 +0900

    [Bronze II] Title: 블랙잭, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,21 +1,15 @@
 import sys
-
 input = sys.stdin.readline
 
 n, m = map(int,input().split())
-nums = list(map(int,input().split()))
-near = m
-m1 = 0
+nums = sorted(list(map(int,input().split())), reverse = True)
 
+m_n = 0
 for i in range(n-2):
-    m1 += 1
-    m2 = m1
-    for j in range(m1,n-1):
-        m2 += 1
-        for k in range(m2,n):
-            if nums[i]+nums[j]+nums[k] <= m:
-                sol = m - (nums[i]+nums[j]+nums[k])
-                if 0 <= sol < near:
-                    near = m - (nums[i]+nums[j]+nums[k])
-                    sum_nums = nums[i]+nums[j]+nums[k]
-print(sum_nums)
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            sol = nums[i] + nums[j] + nums[k]
+            if sol <= m:
+                if m_n < sol: m_n = sol
+                break
+print(m_n)
