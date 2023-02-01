cab0ea4995dc88185380c38b4a11013ce6198b20
LEE Minwook <djqmflsk@naver.com>
2023-02-01 13:34:09 +0900

    [Bronze II] Title: 점수 집계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9076. 점수 집계/점수 집계.py
+++ 백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -1,4 +1,3 @@
 for _ in range(int(input())):
-    li = sorted(list(map(int,input().split())))[1:-1]
-    if li[-1] - li[0] >= 4: print('KIN')
-    else: print(sum(li))
+    li = sorted(list(map(int,input().split())))
+    print(sum(li[1:4]) if li[3]-li[1] < 4 else 'KIN')
