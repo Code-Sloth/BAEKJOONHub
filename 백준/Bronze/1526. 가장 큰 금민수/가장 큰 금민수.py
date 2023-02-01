5d5ae76b927134f23124c3c46c40e5bf0507e26c
LEE Minwook <djqmflsk@naver.com>
2023-02-01 13:56:02 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 112 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -1,10 +1,4 @@
-import sys
-input = sys.stdin.readline
-
-n = int(input())
-set_74 = [{'4'},{'7'},{'4','7'},{'7','4'}]
-
-for i in range(n,3,-1):
-    if set(str(i)) in set_74:
+for i in range(int(input()),3,-1):
+    if not str(i).strip('47'):
         print(i)
         break
