4c4db172442e6ebad388cf0d3ae5b0bbcf567250
LEE Minwook <djqmflsk@naver.com>
2023-02-01 13:28:14 +0900

    [Bronze II] Title: 점수 집계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9076. 점수 집계/점수 집계.py
+++ 백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -1,8 +1,4 @@
-import sys
-input = sys.stdin.readline
-from collections import deque
-
 for _ in range(int(input())):
-    li = deque(sorted(list(map(int,input().split())))[1:-1])
+    li = sorted(list(map(int,input().split())))[1:-1]
     if li[-1] - li[0] >= 4: print('KIN')
     else: print(sum(li))
