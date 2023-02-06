896b3b979e912f640afc8502f3da663cf5e7d55f
LEE Minwook <djqmflsk@naver.com>
2023-02-06 13:42:07 +0900

    [Bronze I] Title: 행복한지 슬픈지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
+++ 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
@@ -2,12 +2,10 @@ import sys
 input = sys.stdin.readline
 
 s = input().rstrip()
-s = s.replace(':-)','1')
-s = s.replace(':-(','2')
-s1 = s.count('1')
-s2 = s.count('2')
+s1 = s.count(':-)')
+s2 = s.count(':-(')
 
-if '1' not in s and '2' not in s: print('none')
+if ':-)' not in s and ':-(' not in s: print('none')
 else:
     if s1 == s2: print('unsure')
     elif s1 > s2: print('happy')
