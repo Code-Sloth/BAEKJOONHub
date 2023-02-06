df18cf8dff3ab93a24addf63fd2756f7ac17cf2c
LEE Minwook <djqmflsk@naver.com>
2023-02-06 13:59:49 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,21 @@
-from sys import stdin
+import sys
+input = sys.stdin.readline
 
-n = int(stdin.readline())
-v = int(stdin.readline())
+n = int(input())
+m = int(input())
+g = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int,input().split())
+    g[a] += [b]
+    g[b] += [a]
+vi = [0] * (n+1)
 
-graph = [ [] for _ in range(n+1) ]
-visited = [0] * (n+1)
+def dfs(x):
+    vi[x] = 1
+    for i in g[x]:
+        if not vi[i]:
+            vi[i] = 1
+            dfs(i)
+    return vi.count(1)-1
 
-for i in range(v) : 
-    a, b = map(int, stdin.readline().split())
-
-    graph[a] += [b]
-    graph[b] += [a]
-
-def dfs(k) : 
-    visited[k] = 1
-
-    for nx in graph[k] : 
-        if visited[nx] == 0 : 
-            dfs(nx)
-
-dfs(1)
-print(sum(visited)-1)
+print(dfs(1))
