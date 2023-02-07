383c56974a79a11605410c7ec616801a2288d4b7
LEE Minwook <djqmflsk@naver.com>
2023-02-07 14:46:49 +0900

    [Silver III] Title: 킹, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -1,32 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-def move(kx,ky,bx,by,ord):
-    dx,dy = di[ord]
-    nx,ny = kx+dx, ky+dy
-    if 0 <= nx+dx < 8 and 0 <= ny+dy < 8 and bx == nx and by == ny:
-        bx,by = nx+dx, ny+dy
-    if 0 <= nx < 8 and 0 <= ny < 8:
-        if bx != nx or by != ny:
+def move(kx,ky,bx,by,order):
+    dx,dy = d[order]
+    nx,ny = kx + dx, ky + dy
+    nnx,nny = nx + dx, ny + dy
+
+    if 0 < nnx < 9 and 0 < nny < 9 and bx == nx and by == ny:
+            bx,by = nnx,nny
+    if 0 < nx < 9 and 0 < ny < 9:
+        if nx != bx or ny != by:
             kx,ky = nx,ny
     return kx,ky,bx,by
 
-tr = str.maketrans('ABCDEFGH','12345678')
-tr_T = str.maketrans('12345678','ABCDEFGH')
-tr_S = str.maketrans('01234567','87654321')
-di = {'R':(0,1),'L':(0,-1),'B':(1,0),'T':(-1,0),
-      'RT':(-1,1),'LT':(-1,-1),'RB':(1,1),'LB':(1,-1)}
 k,b,n = input().split()
-g = [[0]*8 for _ in range(8)]
-k = k.translate(tr)
-b = b.translate(tr)
-kx,ky = 8-int(k[1]),int(k[0])-1
-bx,by = 8-int(b[1]),int(b[0])-1
+d = {'R':(1,0),'L':(-1,0),'B':(0,-1),'T':(0,1),
+      'RT':(1,1),'LT':(-1,1),'RB':(1,-1),'LB':(-1,-1)}
+kx,ky = ord(k[0])-64,int(k[1])
+bx,by = ord(b[0])-64,int(b[1])
 
-for _ in range(int(n)):
+for i in range(int(n)):
     order = input().rstrip()
     kx,ky,bx,by = move(kx,ky,bx,by,order)
 
-k = str((ky+1)).translate(tr_T) + str(kx).translate(tr_S)
-b = str((by+1)).translate(tr_T) + str(bx).translate(tr_S)
-print(k,b)
+k = chr(kx+64) + str(ky)
+b = chr(bx+64) + str(by)
+
+print('\n'.join((k,b)))
