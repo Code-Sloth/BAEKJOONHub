0e8c5e83a96f1ca355664888049747cf4908c4b6
LEE Minwook <djqmflsk@naver.com>
2023-02-08 20:17:32 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -2,11 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-d = [0] * (n+1)
-di = {1:1,2:1}
-def f(x):
-    if x in di: return di[x]
-    if d[x] != 0: return d[x]
-    d[x] = f(x-1) + f(x-2)
-    return d[x]
-print(f(n))
+d = [0] * (n+2)
+d[1] = 1
+d[2] = 1
+for i in range(3,n+2):
+    d[i] = d[i-1] + d[i-2]
+print(d[n])
