85fd0499fa7837cbf3f7065dbaaf43f02a98f84d
LEE Minwook <djqmflsk@naver.com>
2023-02-08 23:11:17 +0900

    [Silver IV] Title: 빙고, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -8,7 +8,7 @@ di = {}
 for i in range(5):
     for j in range(5):
         di[g[i][j]] = i,j
-
+        
 l = [0] * 12
 t = 0
 for ord in order:
@@ -17,16 +17,11 @@ for ord in order:
         t += 1
         if b[0] == b[1]: l[0] += 1
         if b[0] + b[1] == 4: l[1] += 1
-        if b[0] == 0: l[2] += 1
-        if b[0] == 1: l[3] += 1
-        if b[0] == 2: l[4] += 1
-        if b[0] == 3: l[5] += 1
-        if b[0] == 4: l[6] += 1
-        if b[1] == 0: l[7] += 1
-        if b[1] == 1: l[8] += 1
-        if b[1] == 2: l[9] += 1
-        if b[1] == 3: l[10] += 1
-        if b[1] == 4: l[11] += 1
+        cnt3 = -5
+        for cnt1 in range(2):
+            cnt3 += 5
+            for cnt2 in range(5):
+                if b[cnt1] == cnt2: l[cnt2+cnt3+2] += 1
         if l.count(5) > 2:
             print(t)
             exit()
