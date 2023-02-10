6e6af594aed54c59d120e365b2856152d7fd00a7
LEE Minwook <djqmflsk@naver.com>
2023-02-10 20:41:45 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,17 +1,7 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**8)
-
 n = int(input())
-d = [0] * (n+1)
 
-def f(x):
-    if x == 0:
-        return 0
-    elif x == 1:
-        return 1
-    if d[x] != 0:
-        return d[x]
-    d[x] = f(x-1) + f(x-2)
-    return d[x]
+di = {0:0,1:1}
+def f(n):
+    if n in di: return n
+    else: return f(n-2) + f(n-1)
 print(f(n))
