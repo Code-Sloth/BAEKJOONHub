b361a2a77e03830288bcb1955b6c8ed3c768f2f2
LEE Minwook <djqmflsk@naver.com>
2023-02-10 20:27:32 +0900

    [Silver III] Title: 피보나치 수의 개수, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/6571. 피보나치 수의 개수/피보나치 수의 개수.py
+++ 백준/Silver/6571. 피보나치 수의 개수/피보나치 수의 개수.py
@@ -1,14 +1,11 @@
-d = [1,2]
-n = 2
-while d[-1] <= 10**100:
-    d.append(d[n-1] + d[n-2])
-    n += 1
-
 while 1:
     n, m = map(int,input().split())
     if n == 0 and m == 0: break
     t = 0
-    for i in d:
-        if n <= i <= m:
-            t += 1
-    print(t)
+    a = 1
+    b = 1
+    while 1:
+        a,b = b,a+b
+        if a >= n: t += 1
+        if a > m: break
+    print(t-1)
