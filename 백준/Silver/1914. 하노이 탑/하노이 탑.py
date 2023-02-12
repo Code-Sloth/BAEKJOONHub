61aace8043fdaa95d7d41d68eea51bb68da388da
LEE Minwook <djqmflsk@naver.com>
2023-02-12 14:37:17 +0900

    [Silver I] Title: 하노이 탑, Time: 856 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+def hanoi(n,st,end,mid):
+
+    if n == 1: 
+        print(st,end)
+        return
+    hanoi(n-1,st,mid,end)
+    print(st,end)
+ 
+    hanoi(n-1,mid,end,st)
+
+if n > 20:
+    print(2**n-1)
+else:
+    print(2**n-1)
+    hanoi(n,1,3,2)
