8328ed9b886229fe8ee26303e4491c5cb3432c5a
LEE Minwook <djqmflsk@naver.com>
2023-02-15 11:37:55 +0900

    [Silver II] Title: 222-풀링, Time: 740 ms, Memory: 75804 KB -BaekjoonHub

---
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
+
+
+n = int(input())
+g = [list(map(int,input().split())) for _ in range(n)]
+
+def divide(n,arr):
+    global li
+    if n == 1:
+        return
+    li = [[0]* (n//2) for _ in range(n//2)]
+    for i in range(0,n,2):
+        for j in range(0,n,2):
+            mx = []
+            for i2 in range(2):
+                for j2 in range(2):
+                    mx.append(arr[i+i2][j+j2])
+            li[i//2][j//2] = sorted(mx)[2]
+    divide(n//2,li)
+
+divide(n,g)
+print(li[0][0])
