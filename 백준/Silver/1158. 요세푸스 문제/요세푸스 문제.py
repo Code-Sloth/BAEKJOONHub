0ca9b23b1ecd41bbc9972d1fb359efe08cf16bb0
LEE Minwook <djqmflsk@naver.com>
2023-02-16 16:27:48 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -3,15 +3,12 @@ input = sys.stdin.readline
 from collections import deque
 
 n, k = map(int,input().split())
-li = [0] * n
+q = deque([i for i in range(1,n+1)])
 li_q = [0] * n
-
-for i in range(n):
-    li[i] = i+1
-q = deque(li)
 
 for j in range(n):
     q.rotate(-(k-1))
-    li_q[j] = str(q.popleft())
+    li_q[j] = q.popleft()
 
-print(f'<{", ".join(li_q)}>')
+print('<',end='')
+print(*li_q,sep=', ',end='>')
