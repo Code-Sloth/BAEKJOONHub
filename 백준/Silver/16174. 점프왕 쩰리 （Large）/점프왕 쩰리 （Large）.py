e15c01b73d1f4b0087d4a88698ae11d478ce123b
LEE Minwook <djqmflsk@naver.com>
2023-02-19 21:50:13 +0900

    [Silver I] Title: 점프왕 쩰리 (Large), Time: 88 ms, Memory: 34128 KB -BaekjoonHub

---
+++ 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n = int(input())
+g = [list(map(int,input().split())) for _ in range(n)]
+vi = [[0]*n for _ in range(n)]
+
+def bfs(x,y):
+    q = deque([(x,y)])
+    while q:
+        x,y = q.popleft()
+        d = g[x][y]
+        if d > 0:
+            for dx,dy in [(0,d),(d,0)]:
+                nx,ny = x+dx, y+dy
+                if 0 <= nx < n and 0 <= ny < n and not vi[nx][ny]:
+                    if g[nx][ny] == -1:
+                        return print('HaruHaru')
+                    vi[nx][ny] = 1
+                    q.append((nx,ny))
+    return print('Hing')
+
+bfs(0,0)
