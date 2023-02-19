f983008645945c17ca02396cf770ce9609ca3b84
LEE Minwook <djqmflsk@naver.com>
2023-02-19 21:58:33 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -1,10 +1,9 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**8)
+sys.setrecursionlimit(10**6)
 
 n = int(input())
 g = [list(map(int,input().split())) for _ in range(n)]
-t = 0
 
 def dfs(x,y):
     global t
@@ -14,9 +13,8 @@ def dfs(x,y):
             nx,ny = x+dx, y+dy
             if 0 <= nx < n and 0 <= ny < n:
                 if g[nx][ny] == -1:
-                    t = 1
-                    return
+                    print('HaruHaru')
+                    sys.exit()
                 else: dfs(nx,ny)
-
-dfs(0,0)
-print('HaruHaru') if t else print('Hing')
+    return 'Hing'
+print(dfs(0,0))
