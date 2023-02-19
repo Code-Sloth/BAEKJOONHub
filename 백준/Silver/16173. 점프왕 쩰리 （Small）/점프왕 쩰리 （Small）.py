13ce44e99c7c353109cc56acd4dc8982a19d572c
LEE Minwook <djqmflsk@naver.com>
2023-02-19 21:28:02 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
+
+n = int(input())
+g = [list(map(int,input().split())) for _ in range(n)]
+t = 0
+
+def dfs(x,y):
+    global t
+    if g[x][y] > 0:
+        d = g[x][y]
+        for dx,dy in [(0,d),(d,0)]:
+            nx,ny = x+dx, y+dy
+            if 0 <= nx < n and 0 <= ny < n:
+                if g[nx][ny] == -1:
+                    t = 1
+                else: dfs(nx,ny)
+
+dfs(0,0)
+print('HaruHaru') if t else print('Hing')
