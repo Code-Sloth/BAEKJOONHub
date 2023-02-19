78f44f57cbe46898e57a26602ad9d035bd16de8b
LEE Minwook <djqmflsk@naver.com>
2023-02-19 22:11:02 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -6,7 +6,6 @@ n = int(input())
 g = [list(map(int,input().split())) for _ in range(n)]
 
 def dfs(x,y):
-    global t
     if g[x][y] >= 1:
         for dx,dy in [(0,g[x][y]),(g[x][y],0)]:
             nx,ny = x+dx, y+dy
