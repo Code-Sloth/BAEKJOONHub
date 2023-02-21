b524cb785155582dd9de3b7bcd1d67558df66149
LEE Minwook <djqmflsk@naver.com>
2023-02-21 20:06:03 +0900

    [Silver II] Title: 유기농 배추, Time: 52 ms, Memory: 31636 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,28 +1,27 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**8)
+sys.setrecursionlimit(10**6)
 
 d = [(1,0),(-1,0),(0,1),(0,-1)]
+
 def dfs(x,y):
-    if x < 0 or x >= n or y < 0 or y >= m:
-        return False
-    if g[x][y]:
-        g[x][y] = 0
-        for dx, dy in d:
-            nx, ny = x + dx, y + dy
+    for dx,dy in d:
+        nx,ny = x+dx, y+dy
+        if 0 <= nx < n and 0 <= ny < m and g[nx][ny]:
+            g[nx][ny] = 0
             dfs(nx,ny)
-        return True
-    return False
+    return 1
 
 for _ in range(int(input())):
-    m,n,k = map(int,input().split())
+    n, m, k = map(int,input().split())
     g = [[0]*m for __ in range(n)]
-    for __ in range(k):
-        x,y = map(int,input().split())
-        g[y][x] = 1
-    result = 0
+    for ___ in range(k):
+        a, b = map(int,input().split())
+        g[a][b] = 1
+    t = 0
     for i in range(n):
         for j in range(m):
             if g[i][j]:
-                result += dfs(i,j)
-    print(result)
+                g[i][j] = 0
+                t += dfs(i,j)
+    print(t)
