76aafd8c9679ce9ebceeda2a2bf262964166342c
LEE Minwook <djqmflsk@naver.com>
2023-02-23 17:47:28 +0900

    [Silver II] Title: 촌수계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,28 +1,23 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
+sys.setrecursionlimit(10**8)
 
 n = int(input())
 st, end = map(int,input().split())
 m = int(input())
 g = [[] for _ in range(n+1)]
-dis = [0] * (n+1)
+vi = [0] * (n+1)
 
 for _ in range(m):
     a,b = map(int,input().split())
     g[a] += [b]
     g[b] += [a]
 
-def bfs(x):
-    q = deque([x])
-    dis[x] = 1
-    while q:
-        x = q.popleft()
-        if x == end: print(dis[x]-1); return 1
-        for nx in g[x]:
-            if not dis[nx]:
-                dis[nx] = dis[x] + 1
-                q.append(nx)
-    return 0
+def dfs(x,t):
+    vi[x] = 1
+    if x == end: print(t); sys.exit()
+    for nx in g[x]:
+        if not vi[nx]:
+            dfs(nx,t+1)
 
-if not bfs(st):print(-1)
+if not dfs(st,0): print(-1)
