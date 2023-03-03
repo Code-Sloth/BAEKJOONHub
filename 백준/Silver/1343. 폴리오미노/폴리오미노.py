12c2afdd2316741f66236ea8b06560dd52cb12d5
LEE Minwook <djqmflsk@naver.com>
2023-03-03 21:01:47 +0900

    [Silver V] Title: 폴리오미노, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,27 +1,9 @@
-import sys
-input = sys.stdin.readline
+s = input()
 
-s = input().rstrip()
-result = ''
+s = s.replace("XXXX", "AAAA")
+s = s.replace("XX", "BB")
 
-t = 0
-for i in range(len(s)):
-    if s[i] == 'X':
-        t += 1
-    else:
-        if t % 2 == 1:
-            print(-1)
-            sys.exit()
-        t = 0
-        result += '.'
-    if i == len(s)-1 and t % 2 == 1 and s[i] == 'X':
-        print(-1)
-        sys.exit()
-    if t == 2:
-        result += 'BB'
-    elif t == 4:
-        result = result[:-2] + result[-2:].replace('B','A')
-        result += 'AA'
-        t = 0
-
-print(result)
+if 'X' in s:
+    print(-1)
+else:
+    print(s)
