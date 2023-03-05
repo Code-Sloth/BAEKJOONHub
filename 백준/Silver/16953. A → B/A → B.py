d3e9bab1a0d2bd49aa7f78640bfe8f93b3d9db03
LEE Minwook <djqmflsk@naver.com>
2023-03-05 18:08:13 +0900

    [Silver II] Title: A → B, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,15 +1,12 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
 a,b = map(int,input().split())
 
 def dfs(a,t):
-    if a > b:
-        return
-    if a == b:
-        print(t)
-        sys.exit()
-
+    if a > b:return
+    if a == b: print(t); sys.exit()
     dfs(a*2,t+1)
     dfs(a*10+1,t+1)
 
