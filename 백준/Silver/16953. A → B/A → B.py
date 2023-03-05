6e2474de6b7935415de23abf160e26dd58781840
LEE Minwook <djqmflsk@naver.com>
2023-03-05 18:06:59 +0900

    [Silver II] Title: A → B, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+a,b = map(int,input().split())
+
+def dfs(a,t):
+    if a > b:
+        return
+    if a == b:
+        print(t)
+        sys.exit()
+
+    dfs(a*2,t+1)
+    dfs(a*10+1,t+1)
+
+dfs(a,1)
+print(-1)
