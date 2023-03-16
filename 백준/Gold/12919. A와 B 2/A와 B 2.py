7c63410684518472202736be8d5b23fe70191594
LEE Minwook <djqmflsk@naver.com>
2023-03-16 17:59:33 +0900

    [Gold V] Title: A와 B 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+s = input().rstrip()
+t = input().rstrip()
+ls = len(s)
+lt = len(t)
+
+def dfs(t,lt):
+    if t==s: print(1); sys.exit()
+    if lt < ls: return
+
+    if t[-1]=='A':
+        dfs(t[:-1],lt-1)
+    if t[0]=='B':
+        dfs(t[1:][::-1],lt-1)
+
+dfs(t,lt)
+print(0)
