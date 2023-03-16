75bea481f5de1d67272a1e1e319fb3348c62f098
LEE Minwook <djqmflsk@naver.com>
2023-03-16 16:35:33 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 112 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+d, k = map(int,input().split())
+
+fibo = [0] * (d+1)
+fibo[1] = fibo[2] = 1
+
+while fibo[d] != k:
+    for i in range(3,d+1):
+        fibo[i] = fibo[i-1] + fibo[i-2]
+    
+    if fibo[d] < k:
+        fibo[2] += 1
+    elif fibo[d] > k:
+        fibo[1] += 1
+        fibo[2] = fibo[1]
+
+print(fibo[1],fibo[2],sep = '\n')
