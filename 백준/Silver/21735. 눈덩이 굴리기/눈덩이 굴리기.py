f7c206f248b991b1ddf0550e9f853c1595f25f74
LEE Minwook <djqmflsk@naver.com>
2023-03-20 17:00:21 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+snow = [0] + list(map(int,input().split()))
+maxsize = -1
+
+def dfs(x, t, size):
+    global maxsize
+    if t < 0: return
+
+    if t >= 0:
+        maxsize = max(maxsize,size)
+    if x+1 <= n:
+        dfs(x+1, t-1, size+snow[x+1])
+    if x+2 <= n:
+        dfs(x+2, t-1, size//2+snow[x+2])
+
+dfs(0,m,1)
+print(maxsize)
