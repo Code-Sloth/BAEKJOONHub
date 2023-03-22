b19b75779f818db4500e385fdc55aab0c02e54d2
LEE Minwook <djqmflsk@naver.com>
2023-03-22 23:55:10 +0900

    [Silver I] Title: 에너지 모으기, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+n = int(input())
+g = list(map(int,input().split()))
+vi = [0] * n
+
+mx = 0
+def dfs(su, ls):
+    global mx
+
+    if ls == 2:
+        mx = max(mx,su)
+        return
+
+    for i in range(1,ls-1):
+        mu = g[i-1] * g[i+1]
+        p = g.pop(i)
+        dfs(su + mu ,ls-1)
+        g.insert(i,p)
+
+dfs(0,n)
+print(mx)
