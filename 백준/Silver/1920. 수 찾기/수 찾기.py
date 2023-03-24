7adca61c91b1bbb362a3f04d2f3b8f53f707ae30
LEE Minwook <djqmflsk@naver.com>
2023-03-24 22:30:14 +0900

    [Silver IV] Title: 수 찾기, Time: 432 ms, Memory: 46736 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -2,20 +2,25 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-g = sorted(map(int,input().split()))
+g = sorted(list(map(int,input().split())))
 m = int(input())
-mg = map(int, input().split())
+mg = list(map(int,input().split()))
 
-def binary(a,st,ed):
-    if st > ed: return
-    mid = (st+ed)//2
+def func(a):
+    st = 0
+    ed = n-1
 
-    if g[mid] == a:
-        return 1
-    elif g[mid] < a:
-        return binary(a,mid+1,ed)
-    else:
-        return binary(a,st,mid-1)
+    while st <= ed:
+        mid = (st+ed) // 2
+        if g[mid] == a:
+            return 1
+        elif g[mid] < a:
+            st = mid + 1
+        else:
+            ed = mid - 1
 
 for i in mg:
-    print(1) if binary(i,0,n-1) else print(0)
+    if func(i):
+        print(1)
+    else:
+        print(0)
