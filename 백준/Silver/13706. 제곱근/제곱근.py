654a9cc001cbb139dd5be175251ebbf6a990af12
LEE Minwook <djqmflsk@naver.com>
2023-03-24 22:47:00 +0900

    [Silver IV] Title: 제곱근, Time: 44 ms, Memory: 33508 KB -BaekjoonHub

--- 백준/Silver/13706. 제곱근/제곱근.py
+++ 백준/Silver/13706. 제곱근/제곱근.py
@@ -1,18 +1,2 @@
-import sys
-input = sys.stdin.readline
-
-
-n = int(input())
-l = 0
-r = n
-
-while l <= r:
-    m = (l+r)//2
-    
-    if m**2 == n:
-        print(m)
-        break
-    elif m**2 < n:
-        l = m + 1
-    else:
-        r = m - 1
+import math
+print(math.isqrt(int(input())))
