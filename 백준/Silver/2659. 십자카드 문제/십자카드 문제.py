00f8ea79c35ae5478b9d050ca5db250f75d1bd53
LEE Minwook <djqmflsk@naver.com>
2023-04-11 22:13:29 +0900

    [Silver III] Title: 십자카드 문제, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2659. 십자카드 문제/십자카드 문제.py
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.py
@@ -1,20 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-g = list(map(int, input().split()))
+g = input().split()
 
 def func(n):
-    min = int(''.join(map(str, n)))
+    min = int(n)
     for i in range(1,4):
-        tmp = int(''.join(map(str, n[i:]+n[:i])))
-        if min > tmp:
-            min = tmp
+        a = int(''.join(n[i:] + n[:i]))
+        if min > a:
+            min = a
     return min
 
-num = func(g)
-cnt = 1
-
-for i in range(1111, num):
-    if '0' not in list(str(i)) and i == func(list(map(int, str(i)))):
-        cnt += 1
-print(cnt)
+t = 1
+for i in range(1111, func(''.join(g))):
+    if '0' not in list(str(i)) and i == func(str(i)):
+        t += 1
+print(t)
