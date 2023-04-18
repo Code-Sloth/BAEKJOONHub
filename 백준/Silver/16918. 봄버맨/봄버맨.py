1bbadc366a7adb8f2f785e8a9564a224abf9fbec
LEE Minwook <djqmflsk@naver.com>
2023-04-18 23:23:50 +0900

    [Silver I] Title: 봄버맨, Time: 1676 ms, Memory: 35988 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -6,25 +6,34 @@ g = [list(input().rstrip()) for _ in range(r)]
 d = [(1,0),(-1,0),(0,1),(0,-1)]
 n -= 1
 
-while 1:
-    se = set()
+def func1():
     for i in range(r):
         for j in range(c):
             if g[i][j] == 'O':
                 se.add((i,j))
 
-    if not n: break
-
+def func2():
     g = [['O']*c for _ in range(r)]
+    return g
 
-    n -= 1
-    if not n: break
-
+def func3():
     for x,y in se:
         g[x][y] = '.'
         for dx,dy in d:
             if 0 <= x+dx < r and 0 <= y+dy < c:
                 g[x+dx][y+dy] = '.'
+
+while 1:
+    se = set()
+    
+    func1()
+    if not n: break
+
+    g = func2()
+    n -= 1
+    if not n: break
+
+    func3()
     n -= 1
     if not n: break
 
