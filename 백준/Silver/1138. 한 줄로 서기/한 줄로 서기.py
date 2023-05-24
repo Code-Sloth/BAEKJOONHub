040b3ce2fa663993fd19dd91012cbefd64b89710
LEE Minwook <djqmflsk@naver.com>
2023-05-24 23:21:52 +0900

    [Silver II] Title: 한 줄로 서기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+g = list(map(int,input().split()))
+index = [0] * n
+
+for i in range(n):
+    for j in range(n):
+        if not g[i] and not index[j]:
+            index[j] = i+1
+            break
+        if index[j] == 0:
+            g[i] -= 1
+
+print(*index)
