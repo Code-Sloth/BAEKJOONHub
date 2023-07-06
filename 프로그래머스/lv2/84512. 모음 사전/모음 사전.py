b68d058f825d0cfb975b8ace5cae69b0be7f2b40
LEE Minwook <djqmflsk@naver.com>
2023-07-06 19:59:49 +0900

    [level 2] Title: 모음 사전, Time: 2.38 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,12 +1,25 @@
-from itertools import product
+cnt = 0
+answer = ''
+def dfs(q,word):
+    global cnt, answer
+    if q == word:
+        answer = cnt
+        return
+    
+    if len(q) == 5: return
+
+    li = ['A','E','I','O','U']
+    for i in range(5):
+        cnt += 1
+        q.append(li[i])
+        dfs(q,word)
+        q.pop()
+    
+    return
 
 def solution(word):
-    g = []
-    
-    for i in range(1, 6):
-        for c in product(['A', 'E', 'I', 'O', 'U'], repeat=i):
-            pro = ''.join(list(c))
-            g.append(pro)
+    word = list(word)
+    q = []
+    dfs(q,word)
 
-    g.sort()
-    return g.index(word) + 1
+    return answer
